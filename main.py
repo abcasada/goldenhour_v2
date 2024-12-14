d1896c668fbe5d5364ec6bac94b41de2193becea
abcasada <abcasada@gmail.com>
2024-12-13 23:02:22 -0600

    change output to hours

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import csv #todo: use pandas for xlsx
 
 PRECISION = 1 # minutes
 
-def twilight_minutes_day(latitude, date):
+def twilight_hours_day(latitude, date):
     location = LocationInfo(name="Custom Location", region="Custom Region", latitude=latitude, longitude=0)
     start_time = datetime.combine(date, datetime.min.time())
     end_time = datetime.combine(date, datetime.max.time())
@@ -20,26 +20,26 @@ def twilight_minutes_day(latitude, date):
             total_minutes += PRECISION
         current_time += timedelta(minutes=PRECISION)
     
-    return total_minutes
+    return round(total_minutes / 60, 2)
 
-def twilight_minutes_year(latitude):
+def twilight_hours_year(latitude):
     data = []
 
     for x in range(365):
         date = datetime(2023,1,1) + timedelta(x) # or timedelta(days=x)?
-        minutes_in_range = twilight_minutes_day(latitude, date)
-        data.append([date.strftime('%Y-%m-%d'), latitude, minutes_in_range])
+        hours_in_range = twilight_hours_day(latitude, date)
+        data.append([date.strftime('%Y-%m-%d'), latitude, hours_in_range])
     
     return data
 
 def process_latitude(latitude):
-    return twilight_minutes_year(latitude)
+    return twilight_hours_year(latitude)
 
 def main():
     start_time = datetime.now()
     latitudes = [59.91, 59.13, 59.97, 61.9, 63.25, 65.46, 66.74, 67.96, 69.49, 70.51, 70.2, 70.2, 68.55, 65.32, 62.52, 60.99, 59.91]
     timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
-    filename = f"sun_elevation_minutes_{timestamp}.csv"
+    filename = f"goldenhour_duration_{timestamp}.csv"
     
     with multiprocessing.Pool() as pool:
         results = pool.map(process_latitude, latitudes)
@@ -48,10 +48,10 @@ def main():
     for data in results:
         for entry in data:
             date = entry[0]
-            minutes_in_range = entry[2]
+            hours_in_range = entry[2]
             if date not in all_data:
                 all_data[date] = {}
-            all_data[date][entry[1]] = minutes_in_range
+            all_data[date][entry[1]] = hours_in_range
     
     with open(filename, mode='w', newline='') as file:
         writer = csv.writer(file)
